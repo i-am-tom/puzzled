3a426697742db70805566621e9bd2aaa486e8699
ArvedFriedemann <arved.friedemann@stoppe.de>
2023-04-14 00:52:49 +0200

    New version of CCC

--- CabalProject/app/Main.hs
+++ CabalProject/app/Main.hs
@@ -1,51 +1,151 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
-{-# LANGUAGE Unsafe #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Main (main) where
 
+import Control.Category (Category ((.), id), (<<<))
+import Data.IORef (IORef)
 import Data.Kind (Type)
-import Data.Unique (Unique)
-import Prelude
-import System.IO.Unsafe (unsafePerformIO)
-import System.Mem.StableName (StableName, makeStableName)
-import Type.Reflection ((:~~:) (HRefl), Typeable, eqTypeRep, typeOf)
+import Data.Functor.Identity (Identity (Identity, runIdentity))
+import Prelude hiding (curry, fst, id, snd, uncurry)
 
-type Expression :: Type -> Type
-data Expression x where
-  Free
-    :: Unique
-    -> Expression x
+class Category k => CCC k where
+  type Unit   k :: Type
+  type Tensor k :: Type -> Type -> Type
+  type Hom    k :: Type -> Type -> Type
 
-  Apply
-    :: Typeable x
-    => Expression (x -> y)
-    -> Expression x
-    -> Expression y
+  curry   :: k (Tensor k a b) c -> k a (Hom k b c)
+  uncurry :: k a (Hom k b c) -> k (Tensor k a b) c
+  fork    :: k a c -> k a d -> k a (Tensor k c d)
+  exl     :: k (Tensor k a b) a
+  exr     :: k (Tensor k a b) b
 
-  Lambda
-    :: StableName (Expression x -> Expression y)
-    -> (Expression x -> Expression y)
-    -> Expression (x -> y)
+instance CCC (->) where
+  type Unit   (->) = ()
+  type Hom    (->) = (->)
+  type Tensor (->) = (,)
 
-instance Eq (Expression x) where
-  Free   x   == Free   y   = x == y
-  Lambda s _ == Lambda t _ = s == t
-  Apply  f x == Apply  g y
-    = case eqTypeRep (typeOf x) (typeOf y) of
-        Just HRefl -> f == g && x == y
-        Nothing    -> False
+  curry f x y = f (x, y)
+  uncurry f (x, y) = f x y
+  fork f g x = (f x, g x)
+  exl (x, _) = x
+  exr (_, y) = y
 
-  _ == _ = False
+liftCCC :: CCC k => k a b -> k i a -> k i b
+liftCCC = (<<<)
 
-lam :: (Expression x -> Expression y) -> Expression (x -> y)
-lam f = Lambda (unsafePerformIO (makeStableName f)) f
+fst :: CCC k => k a (Tensor k b c) -> k a b
+fst = liftCCC exl
 
-fix :: Expression ((Int -> Int) -> Int)
-fix = lam \f -> Apply f (Apply fix f)
+snd :: CCC k => k a (Tensor k b c) -> k a c
+snd = liftCCC exr
 
--- $> main
-main :: IO ()
-main = print $ fix == fix
+app :: CCC k => k i (Hom k a b) -> k i a -> k i b
+app f x = eval <<< fork f x
+
+eval :: CCC k => k (Tensor k (Hom k a b) a) b
+eval = uncurry id
+
+class CCC k => Cast k x y where
+  cast :: k x y
+
+instance {-# OVERLAPPABLE #-} (CCC k, Cast k b a, Tensor k b i ~ t)
+    => Cast k t a where
+  cast = cast <<< exl
+
+instance CCC k => Cast k a a where
+  cast = id
+
+type Name :: (Type -> Type -> Type) -> Type -> Type -> Type
+type Name k i a = forall x. Cast k x (Tensor k i a) => k x a
+
+lam :: forall k i a b. CCC k => (Name k i a -> k (Tensor k i a) b) -> k i (Hom k a b)
+lam f = curry (f exr_)
+  where
+    exr_ :: Cast k x (Tensor k i a) => k x a
+    exr_ = exr <<< cast @_ @_ @(Tensor k i a)
+
+s :: CCC k => k i (Hom k (Hom k p (Hom k q r)) (Hom k (Hom k p q) (Hom k p r)))
+s = lam \x -> lam \y -> lam \z -> app (app x z) (app y z)
+
+swap :: CCC k => k i (Hom k (Tensor k d c) (Tensor k c d))
+swap = lam \x -> fork (snd x) (fst x)
+
+class CCC k => Propagate k where
+  branch_ :: k (Tensor k x x) x
+  equate_ :: k (Tensor k x x) (Unit k)
+  lift    :: x -> k (Unit k) x
+  not_    :: k x (Unit k)
+
+
+solve :: [Bool] -> (forall k. Propagate k i o) -> i -> Maybe (o, forall k. Propagate k i o)
+--        ^ Infinite list of boolean choices
+
+
+-- x = y; x = 5; y = ?
+-- x = { 1, 2 }; x = y; y = ?
+-- (x = y) -> BOT?
+
+
+branch :: Propagate k => k i x -> k i x -> k i x
+branch this that = liftCCC branch_ (fork this that)
+
+equate :: Propagate k => k i x -> k i x -> k i (Unit k)
+equate this that = liftCCC equate_ (fork this that)
+
+not :: Propagate k => k i x -> k i ()
+not = liftCCC not_
+
+ifThenElse p x y = branch (p >>> x) (not p >>> x)
+split :: Propagate k => k i x -> k i (Maybe (x, k i x))
+
+---
+
+data Reified i o where
+  Id      :: Reified i i
+  Compose :: Reified m o -> Reified i m -> Reified i o
+  Curry   :: Reified (a, b) c -> Reified a (b -> c)
+  Uncurry :: Reified a (b -> c) -> Reified (a, b) c
+
+  Fork :: Reified i a -> Reified i b -> Reified i (a, b)
+  Exl  :: Reified (a, b) a
+  Exr  :: Reified (a, b) b
+
+  Branch  :: Reified (x, x) x
+  Equate  :: Reified (x, x) ()
+  Lift    :: x -> Reified () x
+
+instance Category Reified where
+  id = Id
+  (.) = Compose
+
+instance CCC Reified where
+  type Hom Reified = (->)
+  type Tensor Reified = (,)
+  type Unit Reified = ()
+
+  curry   = Curry
+  uncurry = Uncurry
+  fork    = Fork
+  exl     = Exl
+  exr     = Exr
+
+instance Propagate Reified where
+  branch_ = Branch
+  equate_ = Equate
+  lift    = Lift
+
