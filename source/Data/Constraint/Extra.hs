351c71a5572c894f40683f2484d32eb2eb92b01c
Tom Harding <tomjharding@live.co.uk>
2023-06-08 17:22:12 +0100

    Add constraint implication (not sure this will work).

--- source/Data/Constraint/Extra.hs
+++ source/Data/Constraint/Extra.hs
@@ -1,10 +1,15 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
 
 -- | Extra tools for dealing with constraints.
 module Data.Constraint.Extra
   ( type (&&),
     Trivial,
+
+    type (-->) (..),
   )
 where
 
@@ -21,3 +26,9 @@ type (&&) :: (Type -> Constraint) -> (Type -> Constraint) -> Type -> Constraint
 class (c x, d x) => (c && d) x
 
 instance (c x, d x) => (c && d) x
+
+class (forall x. c x => d x) => c --> d where
+  given :: c x => (d x => r) -> r
+
+instance (forall x. c x => d x) => c --> d where
+  given x = x
