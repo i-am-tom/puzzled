e09d6870fb3bc9f813ab9b006a9b2bab12b6d55b
ArvedFriedemann <arved.friedemann@stoppe.de>
2023-06-14 20:04:42 +0200

    created some constrained monads

--- StackProject/app/ConstrainedMonad.hs
+++ StackProject/app/ConstrainedMonad.hs
@@ -10,11 +10,30 @@ class CMonad c m | m -> c where
 
 class Determine (m :: Type -> Type) (c :: Type -> Constraint) | m -> c
 
-instance (Monad m, Determine m c) => CMonad c m where
-    return = return
-    (>>=) = (>>=)
+--instance (Monad m, Determine m c) => CMonad c m where
+--    return = return
+--    (>>=) = (>>=)
+
+--f :: (CMonad c m, forall x. c x => Eq x) => ... QuantifiedConstraints
 
 test :: (CMonad c m, c Int) => m Int
 test = do 
     x <- return 3
     return (x + 4)
+
+data FCMonad c r where
+    FReturn :: c a => a -> FCMonad c a
+    (:>>=:) :: c a => FCMonad c a -> (a -> FCMonad c b) -> FCMonad c b
+
+instance c ~ d => CMonad c (FCMonad d) where
+    return = FReturn
+    (>>=)  = (:>>=:)
+
+f :: FCMonad Show Int
+f = return 3
+
+{-
+printList :: [a] -> m ()
+printList [] = return ()
+printList (x :: xs) = print x >> printList xs
+-}
