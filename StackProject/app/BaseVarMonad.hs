bb9a45db7cf103499df8eacf589fc036dcc325f2
ArvedFriedemann <arved.friedemann@stoppe.de>
2023-06-15 13:10:23 +0200

    Added IO instance of BaseVarMonad

--- StackProject/app/BaseVarMonad.hs
+++ StackProject/app/BaseVarMonad.hs
@@ -1,6 +1,13 @@
 module BaseVarMonad where
 
+import Data.IORef
+
 class BaseVarMonad m v where
     new :: a -> m (v a)
     read :: v a -> m a
     write :: v a -> a -> m ()
+
+instance BaseVarMonad IO IORef where
+    new = newIORef
+    read = readIORef
+    write = writeIORef
