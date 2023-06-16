f2c5adad052ed7482741bbe24d9ccc56b1bfdc39
ArvedFriedemann <arved.friedemann@stoppe.de>
2023-06-16 22:10:31 +0200

    Fixed lattice instances for constants

--- StackProject/app/Lattice.hs
+++ StackProject/app/Lattice.hs
@@ -1,6 +1,7 @@
 module Lattice where
 
 import DTC
+import Lambda
 
 class Lattice a where
     (/\) :: a -> a -> a
@@ -9,6 +10,8 @@ class Lattice a where
 class (Lattice a) => BoundedJoinSemilattice a where
     top :: a
 
+class (Lattice a) => BoundedMeetSemilattice a where
+    bot :: a
 
 data Top a = Top deriving (Show, Eq, Ord, Functor)
 data Bot a = Bot deriving (Show, Eq, Ord, Functor)
@@ -67,13 +70,44 @@ instance (Functor f, Functor g, Lattice (TB f a), Lattice (TB g a)) => Lattice (
         (_, TBOT) -> TBOT
         (Elem ff, Elem gg)  -> inj $ ff :*: gg
 
-instance (Lattice b) => Lattice (KF b a) where
-    (KF x) /\ (KF y) = KF (x /\ y)
-    (KF x) \/ (KF y) = KF (x \/ y)
+--TODO: Does not propagate bot upwards!
+instance (Eq b, BoundedMeetSemilattice b) => Lattice (TB (KF b) a) where
+    TTOP /\ x = x
+    x /\ TTOP = x
+    TBOT /\ _ = TBOT
+    _ /\ TBOT = TBOT
+    (Elem (KF x)) /\ (Elem (KF y))
+        | (x /\ y) == bot = TBOT
+        | otherwise = inj $ KF (x /\ y)
 
-instance (Lattice a) => Lattice (KRec a) where
-    (KRec x) /\ (KRec y) = KRec (x /\ y)
-    (KRec x) \/ (KRec y) = KRec (x \/ y)
+    TTOP \/ _ = TTOP
+    _ \/ TTOP = TTOP
+    TBOT \/ x = x
+    x \/ TBOT = x
+    (Elem (KF x)) \/ (Elem (KF y))
+        | (x \/ y) == bot = TBOT
+        | otherwise = inj $ KF (x \/ y)
+
+--TODO: Does not propagate bot upwards!
+instance (Eq a, BoundedMeetSemilattice a) => Lattice (TB KRec a) where
+    TTOP /\ x = x
+    x /\ TTOP = x
+    TBOT /\ _ = TBOT
+    _ /\ TBOT = TBOT
+    (Elem (KRec x)) /\ (Elem (KRec y))
+        | (x /\ y) == bot = TBOT
+        | otherwise = inj $ KRec (x /\ y)
+
+    TTOP \/ _ = TTOP
+    _ \/ TTOP = TTOP
+    TBOT \/ x = x
+    x \/ TBOT = x
+    (Elem (KRec x)) \/ (Elem (KRec y))
+        | (x \/ y) == bot = TBOT
+        | otherwise = inj $ KRec (x \/ y)
 
 instance (Lattice (f a), Top :<: f) => BoundedJoinSemilattice (f a) where
     top = inj Top
+
+instance (Lattice (f a), Bot :<: f) => BoundedMeetSemilattice (f a) where
+    bot = inj Bot
